71626b88eb3b85aead0170cb537ed61e4daa0cae
ryanmckay <huzzakawah@gmail.com>
2013-11-18 18:23:17 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	
+	<head> 
+		<link href='http://fonts.googleapis.com/css?family=Share+Tech' rel='stylesheet' type='text/css'>
+		<meta charset="UTF-8">
+		<title>PHP?</title>
+		
+		<!--links the css script for indenting and color-->
+		<link rel = "stylesheet"
+			type = "text/css"
+			href = "first.css" />
+
+	</head>
+
+	<body>
+		<h1> PHP? </h1>
+	
+
+		<p> <?php echo "hello"; ?> </p>
+
+		<p class="back">
+			<a href = "index.html">Home</a>
+		</p>
+		<br>
+	</body>
+</html>
